de7db8323483a7c0d48c8abe1eb0aa78d1f84ecc
kaki-cl <progress48694062@gmail.com>
2018-02-20 16:24:15 +0900

    ページ全体をImageButtonに

--- app/src/main/java/com/example/kakehi/java_android_nock/MainActivity.java
+++ app/src/main/java/com/example/kakehi/java_android_nock/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.kakehi.java_android_nock;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -13,43 +12,27 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private TextView textImageButton;
-    private TextView textButton;
-    private boolean flg = true;
+    private boolean flg = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // TextViewの設定
+        textImageButton = findViewById(R.id.text_view);
+
         // イメージボタンを設定
         ImageButton imageButton = findViewById(R.id.image_button);
-        // TextViewの設定
-        textImageButton = findViewById(R.id.text_imagebutton);
+
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (flg) {
-                    textImageButton.setText(R.string.tapped);
+                    textImageButton.setText(R.string.touchme);
                     flg = false;
                 } else {
-                    textImageButton.setText(R.string.image_button);
-                    flg = true;
-                }
-            }
-        });
-
-        // ボタンを設定
-        Button button = findViewById(R.id.button);
-        // TextViewの設定
-        textButton = findViewById(R.id.text_button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (flg) {
-                    textButton.setText(R.string.tapped);
-                    flg = false;
-                } else {
-                    textButton.setText(R.string.button);
+                    textImageButton.setText(R.string.thanks);
                     flg = true;
                 }
             }
