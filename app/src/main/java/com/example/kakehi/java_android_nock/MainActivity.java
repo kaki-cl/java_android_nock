cf2ef8477e601bdae040962345fc0548e6a507f6
kaki-cl <progress48694062@gmail.com>
2018-02-20 15:32:09 +0900

    ボタンをクリックしたらボタンの大きさが変わる

--- app/src/main/java/com/example/kakehi/java_android_nock/MainActivity.java
+++ app/src/main/java/com/example/kakehi/java_android_nock/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.kakehi.java_android_nock;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
@@ -52,9 +53,9 @@ public class MainActivity extends AppCompatActivity {
         layout.addView(textView);
 
         // ボタンの設定
-        Button button = new Button(this);
+        button = new Button(this);
         button.setText("Button");
-        float scale = getResources().getDisplayMetrics().density;
+        scale = getResources().getDisplayMetrics().density;
         int buttonWidth = (int) (150 * scale);
         LinearLayout.LayoutParams buttonLayoutParams = new LinearLayout.LayoutParams(
                 buttonWidth,
@@ -71,16 +72,38 @@ public class MainActivity extends AppCompatActivity {
 
                 if (flag) {
                     textView.setText("Hello");
+
+                    mode1();
                     flag = false;
                 } else {
                     textView.setText("World");
+
+                    mode2();
                     flag = true;
                 }
             }
         });
-
-
     }
 
+    private void mode1() {
+        buttonWidth = (int) (150 * scale);
+        buttonLayoutParams = new LinearLayout.LayoutParams(
+                buttonWidth, LinearLayout.LayoutParams.WRAP_CONTENT);
+        buttonLayoutParams.setMargins(margins, margins, margins, margins);
+        button.setLayoutParams(buttonLayoutParams);
+    }
 
+    private void mode2() {
+        buttonWidth = (int) (250 * scale);
+        int buttonHeight = (int) (100 * scale);
+        buttonLayoutParams = new LinearLayout.LayoutParams(
+                buttonWidth, buttonHeight);
+        buttonLayoutParams.setMargins((int) (5 * scale),
+                (int) (50 * scale), (int) (50 * scale), (int) (20 * scale));
+
+        Log.v("buttonWidth", String.valueOf(buttonWidth));
+        Log.v("buttonHeight", String.valueOf(buttonHeight));
+        Log.v("buttonLayoutParams", buttonLayoutParams.toString());
+        button.setLayoutParams(buttonLayoutParams);
+    }
 }
