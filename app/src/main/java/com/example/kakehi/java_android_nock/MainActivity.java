1aaf4cc0292aa544be4c842a25c4ecdc319de399
kaki-cl <progress48694062@gmail.com>
2018-02-21 15:24:14 +0900

    変数を画面間で共有した

--- app/src/main/java/com/example/kakehi/java_android_nock/MainActivity.java
+++ app/src/main/java/com/example/kakehi/java_android_nock/MainActivity.java
@@ -6,22 +6,45 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    static final int RESULT_SUBACTIVITY = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button sendButton = findViewById(R.id.send_button);
-        sendButton.setOnClickListener(new View.OnClickListener() {
+        textView = findViewById(R.id.text_view);
+
+        final EditText editText = (EditText)findViewById(R.id.edit_text);
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplication(), SubActivity.class);
-                startActivity(intent);
+                if (editText.getText() != null) {
+                    String str = editText.getText().toString();
+                    intent.putExtra("MESSAGE1", str);
+                    editText.setText("");
+                }
+                startActivityForResult(intent, RESULT_SUBACTIVITY);
             }
         });
+    }
 
+    // SubAcitivity からの返しの結果を受け取る
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        if (resultCode == RESULT_OK && requestCode == RESULT_SUBACTIVITY && null != intent) {
+            String res = intent.getStringExtra("MESSAGE2");
+            textView.setText(res);
+        }
     }
 }
--- app/src/main/java/com/example/kakehi/java_android_nock/SubActivity.java
+++ app/src/main/java/com/example/kakehi/java_android_nock/SubActivity.java
@@ -1,28 +1,48 @@
 package com.example.kakehi.java_android_nock;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-
-/**
- * Created by kakehi on 2018/02/21.
- */
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SubActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private String message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
 
-        Button returnButton = findViewById(R.id.return_button);
-        returnButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        // MainActivityからのデータを受け取る
+        final Intent intent = getIntent();
+        message = intent.getStringExtra("MESSAGE1");
+
+        TextView textView = findViewById(R.id.text_view);
+        textView.setText(message);
+
+        editText = (EditText) findViewById(R.id.edit_text);
+
+        // MainActivity へ戻るボタン
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent1 = new Intent();
+                if (editText.getText() != null) {
+                    //messageはmainで入力されたもの
+                    String str = message + editText.getText().toString();
+                    intent.putExtra("MESSAGE2", str);
+                    editText.setText("");
+                }
+                setResult(RESULT_OK, intent);
                 finish();
             }
         });
+
 
     }
 
