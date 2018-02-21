649b4443173723ab81f4b401eb7c4f47780ac3c5
kaki-cl <progress48694062@gmail.com>
2018-02-21 14:20:19 +0900

    listviewをarrayつかってitemを追加した

--- app/src/main/java/com/example/kakehi/java_android_nock/MainActivity.java
+++ app/src/main/java/com/example/kakehi/java_android_nock/MainActivity.java
@@ -1,49 +1,65 @@
 package com.example.kakehi.java_android_nock;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.Locale;
 
-/**
- * Created by kakehi on 2018/02/19.
- */
 public class MainActivity extends AppCompatActivity {
 
-    private LinearLayout linearLayout;
-    private LayoutInflater inflater;
-    private boolean flag = false;
+    private static final String[] texts = {
+            // Globe Decade の楽曲リストより
+            "Feel Like dance",
+            "Joy to the love (globe)",
+            "SWEET PAIN",
+            "DEPARTURES (RADIO EDIT)",
+            "FREEDOM (RADIO EDIT)",
+            "Is this love",
+            "Can't Stop Fallin' in Love",
+            "FACE",
+            "FACES PLACES",
+            "Anytime smokin' cigarette",
+            "Wanderin' Destiny",
+            "Love again",
+            "wanna Be A Dreammaker",
+            "Sa Yo Na Ra",
+            "sweet heart",
+            "Perfume of love",
+            "MISS YOUR BODY",
+            "still growin' up",
+            "biting her nails",
+            "とにかく無性に…"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        linearLayout = findViewById(R.id.linear_layout);
+        ArrayAdapter<String> arrayAdapter =
+                new ArrayAdapter<>(this, R.layout.list);
 
-        TextView textView = findViewById(R.id.text);
-        textView.setText(R.string.contents);
+        ListView listView = findViewById(R.id.listview);
 
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        for (String str: texts) {
+            arrayAdapter.add(str);
+        }
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                if (!flag) {
-                    getLayoutInflater().inflate(R.layout.inflate_layout, linearLayout);
-                    flag = true;
-                } else {
-                    linearLayout.removeAllViews();
-                    flag = false;
-                }
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Toast.makeText(MainActivity.this,
+                String.format(Locale.US, "%sがtapされました。", texts[position]),
+                Toast.LENGTH_LONG).show();
+
             }
         });
+
     }
 }
