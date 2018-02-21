2e52efcabf114beb7fa5e3729fd95b11a3309bea
kaki-cl <progress48694062@gmail.com>
2018-02-21 13:35:25 +0900

    ボタンを配列で複数個実装

--- app/src/main/java/com/example/kakehi/java_android_nock/MainActivity.java
+++ app/src/main/java/com/example/kakehi/java_android_nock/MainActivity.java
@@ -1,41 +1,80 @@
 package com.example.kakehi.java_android_nock;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.View;
-import android.widget.ImageButton;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.util.Locale;
 
 /**
  * Created by kakehi on 2018/02/19.
  */
 public class MainActivity extends AppCompatActivity {
 
-    private TextView textImageButton;
-    private boolean flg = false;
+    private Button button[] = new Button[5];
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // リニアレイアウトの設定
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        layout.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT));
+
+        layout.setGravity(Gravity.CENTER);
+
+        //背景色
+        layout.setBackgroundColor(Color.rgb(0xdd, 0xff, 0xee));
+        setContentView(layout);
+
+        float dp = getResources().getDisplayMetrics().density;
+        int buttonWidh = (int) (250 * dp);
+        int margins = (int) (10 * dp);
+
+
         // TextViewの設定
-        textImageButton = findViewById(R.id.text_view);
+        textView = new TextView(this);
+        String str = "TextView";
+        textView.setText(str);
+        textView.setTextColor(0xff000000);
+        textView.setTextSize(10*dp);
+        layout.addView(textView,
+                new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.WRAP_CONTENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT));
+        // Button[] 配列の設定
+        for (int i = 0; i < button.length ; i++) {
+            button[i] = new Button(this);
+            button[i].setTag(String.valueOf(i));
+            button[i].setText(String.format(Locale.JAPAN, "Button %d", i));
 
-        // イメージボタンを設定
-        ImageButton imageButton = findViewById(R.id.image_button);
+            LinearLayout.LayoutParams buttonLayoutParams =
+                    new LinearLayout.LayoutParams(buttonWidh,
+                            ViewGroup.LayoutParams.WRAP_CONTENT);
+            buttonLayoutParams.setMargins(margins, margins, margins, margins);
 
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (flg) {
-                    textImageButton.setText(R.string.touchme);
-                    flg = false;
-                } else {
-                    textImageButton.setText(R.string.thanks);
-                    flg = true;
+            button[i].setLayoutParams(buttonLayoutParams);
+            layout.addView(button[i]);
+
+            // Listnerをセット
+            button[i].setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
+                    // ViewからTagを取り出す
+                    textView.setText(String.format(Locale.JAPAN,
+                     "Button: %s", view.getTag().toString()));
                 }
-            }
-        });
+            });
+        }
     }
 }
